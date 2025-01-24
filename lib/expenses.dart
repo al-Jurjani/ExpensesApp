edb2547c862112873fc1865eb2f5b2890a3b7c20
Zuhair binFarhan <zuhairfarhan@gmail.com>
2025-01-24 12:33:42 +0500

    init commit.

---
+++ lib/expenses.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// main screen of app
+class Expenses extends StatefulWidget {
+
+}
+
+class _Expenses extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
